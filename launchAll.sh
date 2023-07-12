3a633423cac243319b26c3ef0edd2c82aae30ea7
LabPC <1159218771@qq.com>
2023-07-12 02:05:41 -0700

    71223V1

---
+++ launchAll.sh
@@ -0,0 +1,2 @@
+gnome-terminal --window -t "runDemo01" -x bash -c "./rosLearn/runDemo01.sh;exec bash"
+gnome-terminal --window -t "runDemo02" -x bash -c "./rosLearn/runDemo02.sh;exec bash"
---
+++ runDemo02.sh
@@ -0,0 +1,4 @@
+cd ~/rosLearn/demo02
+source devel/setup.bash 
+roslaunch plumbing_pub_sub start_node.launch &
+sleep 0.5
