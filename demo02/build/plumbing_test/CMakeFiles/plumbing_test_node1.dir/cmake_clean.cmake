71374694109701ae10ae3837b39877ba9ec75313
xxcMU <1159218771@qq.com>
2023-09-07 12:53:48 +0800

    9723V1

---
+++ demo02/build/plumbing_test/CMakeFiles/plumbing_test_node1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/plumbing_test_node1.dir/src/test02_sub_pose.cpp.o"
+  "/home/user/rosLearn/demo02/devel/lib/plumbing_test/plumbing_test_node1.pdb"
+  "/home/user/rosLearn/demo02/devel/lib/plumbing_test/plumbing_test_node1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/plumbing_test_node1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
