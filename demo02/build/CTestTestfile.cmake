397accfecc2870b28087200118541d4bdf422e30
winUbuntu <1159218771@qq.com>
2023-11-02 08:57:59 -0700

    110223V1P154

--- demo02/build/CTestTestfile.cmake
+++ demo02/build/CTestTestfile.cmake
@@ -5,10 +5,12 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("plumbing_my")
 subdirs("plumbing_apis")
 subdirs("plumbing_head")
 subdirs("plumbing_head_src")
 subdirs("plumbing_param_server")
 subdirs("plumbing_pub_sub")
 subdirs("plumbing_server_client")
+subdirs("launch01_basic")
 subdirs("plumbing_test")
---
+++ demo02/build/launch01_basic/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/user/rosLearn/demo02/src/launch01_basic
+# Build directory: /home/user/rosLearn/demo02/build/launch01_basic
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
