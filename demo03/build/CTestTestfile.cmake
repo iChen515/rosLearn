c390aad47186f2acb19f74781c346b2dc13896c9
winUbuntu <1159218771@qq.com>
2023-11-15 06:52:27 -0800

    111523V2P199

--- demo03/build/CTestTestfile.cmake
+++ demo03/build/CTestTestfile.cmake
@@ -6,4 +6,5 @@
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
 subdirs("tf01_static")
+subdirs("tf03_tfs")
 subdirs("tf02_dynamic")
---
+++ demo03/build/tf02_dynamic/CMakeFiles/tf02_dynamic_node1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tf02_dynamic_node1.dir/src/demo02_dynamic_sub.cpp.o"
+  "/home/user/rosLearn/demo03/devel/lib/tf02_dynamic/tf02_dynamic_node1.pdb"
+  "/home/user/rosLearn/demo03/devel/lib/tf02_dynamic/tf02_dynamic_node1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tf02_dynamic_node1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
